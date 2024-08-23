8a04f52c5066c9109d005c8f5f04261ff0955c46
josechavez-bit <77467474@idexperujapon.edu.pe>
2024-08-23 17:23:55 -0500

    mi programa php

---
+++ incrustado-02.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mi tercer programa php</title>
+</head>
+<body>
+    <h1><?php echo "Jose Enrique Chavez Puscan";?></h1>
+</body>
+</html>
---
+++ incrustado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primer programa en PHP</title>
+</head>
+<body>
+    <h1>
+        <?php echo "Hola nuevo mundo desde PHP...."; ?>
+    </h1>
+</body>
+</html>
